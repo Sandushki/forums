5cdd9c774bbac26be369e23ffbaf078dea9f0e64
Sander Mehmet Çelebiler <privaterelay26@gmail.com>
2024-09-09 20:44:39 +0300

    commit main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # SQLite'ı bağlama
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Veri tabanı oluşturma
 db = SQLAlchemy(app)
